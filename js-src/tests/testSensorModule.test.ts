2543d3cea9bbbe748f0b560aa883b21443dd0238
SnehaKondru-git <snehak@ketryx.com>
2024-07-17 16:23:33 -0400

    Update testSensorModule.test.ts

--- js-src/tests/testSensorModule.test.ts
+++ js-src/tests/testSensorModule.test.ts
@@ -1,4 +1,4 @@
-describe('Javascript automated test for sensor module @tests:KXITM20K5H2PQRE8YA9E1Q08TBYR9SN', () => {
+describe('Javascript automated test for sensor module @tests:KXITM1A9B9BCRVK8NBSTS9NKXWFKC1E', () => {
   it('sensor module functions correctly', () => {
     // This would test the sensor module.
     expect(1 + 2).toBe(3);
