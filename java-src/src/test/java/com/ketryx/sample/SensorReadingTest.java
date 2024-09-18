631607565f7e1644ff0bb4baf8f283dee64226b2
Gabriel Pascualy <gabrielp@ketryx.com>
2024-09-18 10:19:38 -0400

    Update SensorReadingTest.java

--- java-src/src/test/java/com/ketryx/sample/SensorReadingTest.java
+++ java-src/src/test/java/com/ketryx/sample/SensorReadingTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SensorReadingTest {
     /**
      * Tests that sensor is read correctly.
-     * @tests:SensorReading
+     * @tests:SensorReadingDS
      * @itemTitle:"Test Sensor Reading (Java)"
      */
     @Test
