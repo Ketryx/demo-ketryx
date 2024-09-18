23be9f9125adc292750401cfc094476a72cf21d4
Gabriel Pascualy <gabrielp@ketryx.com>
2024-09-18 10:15:20 -0400

    Update SensorReading.java

--- java-src/src/main/java/com/ketryx/sample/SensorReading.java
+++ java-src/src/main/java/com/ketryx/sample/SensorReading.java
@@ -3,7 +3,7 @@ package com.ketryx.sample;
 /**
  * Utility class to read sensor.
  *
- * @itemId:KXITM0MT1FR2EN79V7S4WFM1BR082MA
+ * @itemId:SensorReading
  * @itemTitle:"Sensor Reading (Java)"
  * @itemHasParent:spec-sensor-module
  * @itemFulfills:CS-1,KD-20
