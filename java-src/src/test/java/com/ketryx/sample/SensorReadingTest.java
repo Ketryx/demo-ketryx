bd78db3d806943299ed04d8dfc4dc4d8aeb3984f
Gabriel Pascualy <gabrielp@ketryx.com>
2024-10-03 21:24:58 -0400

    Update SensorReadingTest.java

--- java-src/src/test/java/com/ketryx/sample/SensorReadingTest.java
+++ java-src/src/test/java/com/ketryx/sample/SensorReadingTest.java
@@ -1,17 +1,27 @@
 package com.ketryx.sample;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SensorReadingTest {
     /**
      * Tests that sensor is read correctly.
-     * @tests:SensorReadingDS
+     * @tests:SensorReading
      * @itemTitle:"Test Sensor Reading (Java)"
      */
     @Test
     public void sensorReadingTest() {
+        assertEquals(3, SensorReading.readSensor(1, 2));
+    }
+
+    // @itemTitle:"Test Peripheral Reading (Java)" @tests:KD-32
+    // @tests:KD-8
+    // @tests:KD-33
+    @Test
+    @DisplayName("Should return")
+    void createPeripheral() {
         assertEquals(3, SensorReading.readSensor(1, 2));
     }
 }
