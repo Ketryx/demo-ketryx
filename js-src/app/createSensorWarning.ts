c0965267b932dbb68bb5d57139e425de44239a85
demo-ketryx <snehak@ketryx.com>
2024-11-07 11:57:39 -0500

    Update createSensorWarning.ts

--- js-src/app/createSensorWarning.ts
+++ js-src/app/createSensorWarning.ts
@@ -1,11 +1,11 @@
 /**
- * When either of the following conditions is met, the system shall issue a warning notification to the user through an appropriate user interface:
+ * Triggers warnings for detected anomalies or high-risk blockage areas in the coronary arteries, displayed on the clinician’s dashboard. When either of the following conditions is met, the system shall issue a warning notification to the user through an appropriate user interface:
  * a) The sensor reading exceeds the predefined upper or lower limits.
  * b) The latest entered sensor reading is outdated according to the defined maximum allowable time interval.
 
  * @itemId:spec-sensor-reading-warning
- * @itemTitle:"Sensor Reading Warning (Javascript)"
- * @itemFulfills:CS-1,KD-20
+ * @itemTitle:"Alert Module (JavaScript)"
+ * @itemFulfills:KD-20
  * @itemHasParent:spec-sensor-module
  */
 export function createReadingWarning() {
